19af1851d9e16ea48666eaed56258d5db5ff56eb
caylazabel <caylazabel@gmail.com>
2017-01-18 18:16:58 -0800

    working lab 12

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -8,18 +8,21 @@
   repos.requestRepos = function(callback) {
     // done: How would you like to fetch your repos? Don't forget to call the callback.
     $.ajax({
-      url: 'https://api.github.com/user/repos?type=owner',
+      url: 'https:api.github.com/users/caylazabel/repos?type=owner',
       method: 'GET',
       headers: {
-        Authorization: 'token ' + githubToken
+        Authorization: `token ${githubToken}`
+         // Authorization: 'token ' + githubToken
       }
     })
-    callback();
+     .then(data => {
+       repos.all = data;
+       callback();
+     })
   };
 
-
-// REVIEW: Model method that filters the full collection for repos with a particular attribute.
-// You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+    // REVIEW: Model method that filters the full collection for repos with a particular attribute.
+    // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -1,29 +1,26 @@
 'use strict';
-
 (function(module) {
   const repoView = {};
 
-  // REVIEW: Private methods declared here live only within the scope of the wrapping IIFE.
+   // REVIEW: Private methods declared here live only within the scope of the wrapping IIFE.
   const ui = function() {
-    let $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
+     let $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
 
     $about.find('ul').empty();
-    $about.show().siblings().hide();
+   $about.show().siblings().hide();
   };
 
-  // DONE: Remember that new Handlebars template? Let's compile it!
-  // Save the result in this `render` variable.
+ // TODO: Remember that new Handlebars template? Let's compile it!
+
 
   var render = Handlebars.compile($('#repo-template').text());
-  $('#about ul').append(repos.with('name').map(render));
-
   repoView.index = function() {
-    ui();
+     ui();
 
-    // The jQuery `append` method lets us append an entire array of HTML elements at once:
+     // The jQuery `append` method lets us append an entire array of HTML elements at once:
     $('#about ul').append(
-      repos.with('name').map(render) // Want to filter by a different property other than name?
-    );
+       repos.with('name').map(render) // Want to filter by a different property other than name?
+     );
   };
 
   module.repoView = repoView;
